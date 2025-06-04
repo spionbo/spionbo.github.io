aa97b9d35daca59c7c75042aede5f517b882cd93
彭波 <pengbo23812@talkweb.com.cn>
2025-06-04 08:52:01 +0800

    更新配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,18 @@
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
-import { viteBundler } from '@vuepress/bundler-vite'
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
-  lang: 'en-US',
+  lang: "en-US",
 
-  title: 'VuePress',
-  description: 'My first VuePress Site',
+  title: "小焱分享",
+  description: "软件安装和项目教程分享",
 
   theme: defaultTheme({
-    logo: 'https://vuejs.press/images/hero.png',
+    logo: "https://vuejs.press/images/hero.png",
 
-    navbar: ['/', '/get-started'],
+    navbar: ["/", "/get-started"],
   }),
 
   bundler: viteBundler(),
-})
+});
