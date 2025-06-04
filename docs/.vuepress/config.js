d81cefe991c7f5d8e0a6616ff3a540c8095c8453
彭波 <pengbo23812@talkweb.com.cn>
2025-06-04 14:58:15 +0800

    更新提示

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,9 +9,40 @@ export default defineUserConfig({
   description: "软件安装和项目教程分享",
 
   theme: defaultTheme({
-    logo: "https://vuejs.press/images/hero.png",
+    logo: "./images/logo.jpg",
 
-    navbar: ["/", "/get-started"],
+    navbar: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "指南",
+        prefix: "guide/",
+        children: [
+          {
+            text: "安装指南",
+            link: "/guide/README.md",
+          },
+        ],
+      },
+      {
+        text: "软件",
+        link: "/software/",
+      },
+      {
+        text: "更多",
+        prefix: "more/",
+        children: [
+          {
+            text: "关于我",
+            link: "/more/about/README.md",
+          },
+        ],
+      },
+    ],
+
+    contributors: false,
   }),
 
   bundler: viteBundler(),
